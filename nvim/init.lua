f1a80a844b73d49cc17be1833fa2493bd0c45c96
Alex Hollinghead <ach4@princeton.edu>
2026-05-05 11:48:50 -0400

    Disable unused language providers and built-in plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,14 @@
+-- Disable unused providers and built-in plugins
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_gzip = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_tutor = 1
+
 -- Setup Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
